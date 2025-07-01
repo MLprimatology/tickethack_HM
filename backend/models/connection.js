3b44e1d6702ce9ad4433084a07fd53c2562800bb
Mathieu Legrand <mathieu.legrand78@gmail.com>
2025-07-01 10:45:02 +0200

    fichier connection.js

---
+++ backend/models/connection.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb+srv://admin:whhzsSnK9MYMjD0b@cluster0.ckyrlea.mongodb.net/tickethack';
+
+mongoose.connect(connectionString, { connectTimeoutMS: 2000 }).then(() => console.log('Database connected')) .catch(error => console.error(error));
