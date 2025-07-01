6b6b1dfa06d39b1df3a6f7eff52c4bf0c7c4c50f
canadas hugo <canadashcb@gmail.com>
2025-07-01 14:55:37 +0200

    ajout date

--- front/accueil/index.js
+++ front/accueil/index.js
@@ -9,22 +9,41 @@ const formatText = (txt) => {
    return txt[0].toUpperCase() + txt.slice(1).toLowerCase()
 }
 
+/*
+const makeTravelList = (list) => {
+    return list.map(trajet => {`
+        <div class="travelList">
+            <span>${trajet.}</span>
+            <span>ville2</span>
+            <span>heur</span>
+            <span>prix</span>
+            <span class="book">Book</span>
 
+        </div>
+        
+        
+        `
+    })
+}
+
+*/
 
 const searchTrips = async() => {
     const resp = await fetch('http://localhost:3000/trips/find', {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
-		body: JSON.stringify({ departure : formatText(searchByDeparture.value) , arrival : formatText(searchByArrival.value) }),
+		body: JSON.stringify({ departure : formatText(searchByDeparture.value) , arrival : formatText(searchByArrival.value), date : new Date(searchByDate.value) }),
         }
     )
     const data = await resp.json()
     console.log(data)
+
     
 }
 
 
-
+const date = new Date('02-07-2025')
+console.log(date)
 
 
 
