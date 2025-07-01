57dff0de6a870341524194e1434261e9de682526
Mathieu Legrand <mathieu.legrand78@gmail.com>
2025-07-01 11:00:07 +0200

    ajout des fichiers de routes / collections

---
+++ backend/routes/trips.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+const Trip = require('../models/trips')
+const fetch = require("node-fetch");
+
+
+
+
+
+
+
+
+
+module.exports = router;
