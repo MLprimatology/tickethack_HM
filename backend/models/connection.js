f90c70d4870b292aa1dfed791d07f25c3ff18e71
Mathieu Legrand <mathieu.legrand78@gmail.com>
2025-07-02 08:58:24 +0200

    ajout des variables d'environnement dans le fichier .env

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://admin:whhzsSnK9MYMjD0b@cluster0.ckyrlea.mongodb.net/tickethack';
+const connectionString = process.env.CONNECTION_STRING
+
+
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 }).then(() => console.log('Database connected')) .catch(error => console.error(error));
