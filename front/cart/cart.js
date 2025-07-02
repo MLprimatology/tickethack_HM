d5c51dc712ecbbe4f7c854d3bd88cfcce50c9ae7
canadas hugo <canadashcb@gmail.com>
2025-07-02 07:11:03 +0200

    cart fini

--- front/cart/cart.js
+++ front/cart/cart.js
@@ -1,12 +1,22 @@
 const centralContainer = document.getElementById('centralContainer')
+let total;
+let purchase;
+let list
 
+// fonction basiques
 const getFullHours = (date) => {
     const newDate = new Date(date)
     return newDate.getHours() + ':' + (newDate.getMinutes() < 10 ? '0' + newDate.getMinutes() : newDate.getMinutes())
 }
 
-const creatList = (list) => {
+
+
+
+// fonctions complexes
+
+const creatList = () => {
     const trip = list.map(item => {return item.trip})
+    let totalAmount = trip.reduce((acc, b) =>  acc + b.price, 0)
     console.log(list)
     centralContainer.innerHTML = `<p>My cart</p>`
     centralContainer.innerHTML += trip.map(trajet => `
@@ -19,13 +29,24 @@ const creatList = (list) => {
                 `).join('')
         centralContainer.innerHTML += `
             <div id="totalBuy">
-                <div id="total">103</div>
+                <div id="total">Total : ${totalAmount}€</div>
                 <div id="purchase">purchase</div>
             </div>
         `
+    let btnSurppimer = document.querySelectorAll('.supprimer')
+    btnSurppimer.forEach((btn, i )=> btn.addEventListener('click', () => supressToCard(list[i]._id)))
+    total = document.getElementById('total')
+    purchase = document.getElementById('purchase')
+    purchase.addEventListener('click', buy)
+
+
 }
 
-const getCartList = async() => {
+
+// fonctions fetch
+
+// recuperer la liste des trajet en attente de paiement
+const getCartList = async(id) => {
     try{
     const resp = await fetch('http://localhost:3000/trips/cart', {
 		method: 'GET',
@@ -34,13 +55,49 @@ const getCartList = async() => {
      )
     const data = await resp.json()
      console.log(data)
-     creatList(data.trip)
-
-
+     list = data.trip
+     creatList()
 
 } catch(err) {
     console.log(err)
 }
 }
+
+// supprimer un trajet en attente de paiement
+const supressToCard = async(id) => {
+    try {
+    const resp = await fetch('http://localhost:3000/trips/deletecart', {
+        method: 'DELETE',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({tripID : id})
+    })
+    const data = resp.json()
+    console.log(data)
+    getCartList()
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+
+// payer
+const buy = async() => {
+    try {
+        const resp = await fetch('http://localhost:3000/trips/tobook', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+        })
+        const data = await resp.json()
+        console.log(data)
+
+
+
+    } catch(err) {
+        console.log(err)
+    }
+
+}
+
+
 
 getCartList()
